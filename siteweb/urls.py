ce1c9659c888cf6bfc90787ea61d610d8425584a
lucasthierry9 <lucasthierry092@gmail.com>
2025-07-07 02:26:03 -0300

    iniciando o base.html

--- siteweb/urls.py
+++ siteweb/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="inicio"),
+    path("", views.inicio, name="inicio"),
     path("elenco/", views.elenco, name="elenco"),
     path("sobre/", views.sobre, name="sobre"),
 ]
